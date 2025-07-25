e11729f990ad48ef437f1de706b7594b2dad7ab8
robert-barbosa <rob.2612.rob@gmail.com>
2025-07-25 11:10:54 -0300

    priemeira versão.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Digite um número: ");
+        int b = sc.nextInt();
+
+        System.out.print("Digite um número: ");
+        int c = sc.nextInt();
+
+        System.out.print("Digite um número: ");
+        int d = sc.nextInt();
+
+        int diferenca = ((a * b)-(c * d));
+
+        System.out.println("Diferença = " + diferenca);
+
+        sc.close();
+    }
+}
